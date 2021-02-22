a881dea6d8bab9c8dda613d8ea6d669d0c388875
Dan Harrin <git@danharrin.com>
2021-02-22 21:58:47 +0000

    wip

--- app/Filament/Resources/OrderResource/Forms/RelationManagers/ProductsRelationManager.php
+++ app/Filament/Resources/OrderResource/Forms/RelationManagers/ProductsRelationManager.php
@@ -4,14 +4,46 @@ namespace App\Filament\Resources\OrderResource\Forms\RelationManagers;
 
 use Filament\Resources\Forms\Components;
 use Filament\Resources\Forms\Form;
-use Filament\Resources\Forms\RelationManager;
+use Filament\Resources\RelationManager;
 use Filament\Resources\Tables\Columns;
 use Filament\Resources\Tables\Filter;
 use Filament\Tables\Table;
 
 class ProductsRelationManager extends RelationManager
 {
-    protected static $relationship = 'products';
+    public static $actions = [
+        'add',
+        'create',
+        'delete',
+        'edit',
+        'remove',
+    ];
+
+    public static $relationship = 'products';
+
+    public static function form(Form $form)
+    {
+        return $form
+            ->schema([
+                Components\Fieldset::make()->schema([
+                    Components\TextInput::make('name')
+                        ->placeholder('Name')
+                        ->autofocus()
+                        ->required(),
+                    Components\TextInput::make('price')
+                        ->placeholder('Price')
+                        ->type('number')
+                        ->min(0)
+                        ->required(),
+                ]),
+                Components\RichEditor::make('description')
+                    ->placeholder('Description')
+                    ->attachmentDirectory('product-attachments'),
+                Components\TagsInput::make('tags')
+                    ->placeholder('Tags'),
+                Components\FileUpload::make('image')->image(),
+            ]);
+    }
 
     public static function table(Table $table)
     {
