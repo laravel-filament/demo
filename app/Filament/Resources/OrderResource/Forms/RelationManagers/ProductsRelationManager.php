3cd32b2393699f33ce71ff415873c361077d2277
Dan Harrin <git@danharrin.com>
2021-02-26 00:53:54 +0000

    wip

--- app/Filament/Resources/OrderResource/Forms/RelationManagers/ProductsRelationManager.php
+++ app/Filament/Resources/OrderResource/Forms/RelationManagers/ProductsRelationManager.php
@@ -14,12 +14,7 @@ class ProductsRelationManager extends RelationManager
 {
     public static $relationship = 'products';
 
-    // public static $title = 'Related Products';
-
-    public static function getTitle()
-    {
-        return __('Related Products');
-    }
+    public static $title = 'Related Products';
 
     public static function form(Form $form)
     {
