b4f6131140392d2f074b6ff17eb4c82d8c6a141a
Ryan Scherler <ryan@eastslope.studio>
2021-02-28 21:21:46 -0800

    .

--- resources/views/filament/widgets/stats.blade.php
+++ resources/views/filament/widgets/stats.blade.php
@@ -1,18 +1,18 @@
-<div class="col-span-1 lg:col-span-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
+<div class="grid grid-cols-1 col-span-1 gap-4 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
     <x-filament::card>
-        <div class="font-mono text-gray-500 truncate">Total Customers</div>
+        <div class="font-mono text-sm text-gray-500 truncate">Total Customers</div>
 
         <div wire:poll.10s class="text-4xl font-medium">{{ number_format(\App\Models\Customer::count()) }}</div>
     </x-filament::card>
 
     <x-filament::card>
-        <div class="font-mono text-gray-500 truncate">Total Orders</div>
+        <div class="font-mono text-sm text-gray-500 truncate">Total Orders</div>
 
         <div wire:poll.10s class="text-4xl font-medium">{{ number_format(\App\Models\Order::count()) }}</div>
     </x-filament::card>
 
     <x-filament::card>
-        <div class="font-mono text-gray-500 truncate">Total Products</div>
+        <div class="font-mono text-sm text-gray-500 truncate">Total Products</div>
 
         <div wire:poll.10s class="text-4xl font-medium">{{ number_format(\App\Models\Product::count()) }}</div>
     </x-filament::card>
