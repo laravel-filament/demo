540414af44917e52d47e9ef2f82cea0e61f5d57f
Dan Harrin <git@danharrin.com>
2021-03-01 01:59:52 +0000

    poll stats

--- resources/views/filament/widgets/stats.blade.php
+++ resources/views/filament/widgets/stats.blade.php
@@ -2,18 +2,18 @@
     <x-filament::card>
         <div class="font-mono text-gray-500 truncate">Total Customers</div>
 
-        <div class="text-4xl font-medium">{{ number_format(\App\Models\Customer::count()) }}</div>
+        <div wire:poll.10s class="text-4xl font-medium">{{ number_format(\App\Models\Customer::count()) }}</div>
     </x-filament::card>
 
     <x-filament::card>
         <div class="font-mono text-gray-500 truncate">Total Orders</div>
 
-        <div class="text-4xl font-medium">{{ number_format(\App\Models\Order::count()) }}</div>
+        <div wire:poll.10s class="text-4xl font-medium">{{ number_format(\App\Models\Order::count()) }}</div>
     </x-filament::card>
 
     <x-filament::card>
         <div class="font-mono text-gray-500 truncate">Total Products</div>
 
-        <div class="text-4xl font-medium">{{ number_format(\App\Models\Product::count()) }}</div>
+        <div wire:poll.10s class="text-4xl font-medium">{{ number_format(\App\Models\Product::count()) }}</div>
     </x-filament::card>
 </div>
